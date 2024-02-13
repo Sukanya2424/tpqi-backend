4a9cb2eb639ac59d02b45c8112492bf63861f826
Phat <phatnarins63@nu.ac.th>
2024-02-13 14:24:59 +0700

    edit index.js

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ app.use(express.json());
 
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://tpqicompetencyprofilesystem.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
   
 const db = mysql.createConnection(process.env.DATABASE_URL)
 
